32b7c5836162e00cf7306651af7b7bba842ec5a7
Sitthata <aprils96950@gmail.com>
2023-11-22 12:48:08 +0700

    Refactor todoList.js: Remove unused import and update event listeners

--- scripts/todoList.js
+++ scripts/todoList.js
@@ -1,4 +1,4 @@
-import TodoList, { Todo } from "./todo";
+import TodoList from "./todo";
 
 const todoInput = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
@@ -7,25 +7,51 @@ const listTodo = document.getElementById("listTodo")
 const todoList = new TodoList;
 
 addBtn.addEventListener('click', () => {
-    const todoValue = todoInput.value;
+    let todoValue = todoInput.value;
 
-    if (todoValue.trim() === "") alert("Enter todo to proceed!")
-
+    if (!todoValue.trim()) alert("Enter todo to proceed!")
     todoList.addTodo(todoValue);
-    todoList.value = "";
+    todoInput.value = '';
     renderTodo();
 });
+
+listTodo.addEventListener('click', (e) => {
+    const target = e.target;
+    const action = target.dataset.action;
+    const id = parseInt(target.dataset.id);
+
+    switch(action) {
+        case 'remove': 
+            removeTodo(id);
+            break;
+        case 'toggle': 
+            toggle(target);
+            break;
+    }
+})
+
+const removeTodo = (id) => {
+    todoList.removeTodo(id);
+    renderTodo();
+}
+
+const toggle = (target) => {
+    target.classList.toggle('todo-completed');
+    target.textContent = target.classList.contains('todo-completed') ? "Done" : "Not Done";
+}
 
 const renderTodo = () => {
     const todos = todoList.getTodos();
     let html = ''
+    const isComplete = false;
+    const completedClass = isComplete ? 'todo-completed' : '';
     todos.forEach(todo => {
         html += 
         `
-        <div class="todoItem" id="newId">
+        <div class="todoItem ${completedClass}" id="${todo.id}">
             <p>${todo.description}</p>
-            <button>Not done</button>
-            <button>remove</button>
+            <button data-action="toggle" data-id="${todo.id}">Not done</button>
+            <button data-action="remove" data-id="${todo.id}">remove</button>
          </div>
         `
     });
